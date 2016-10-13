983b4eb5f4d666e1a6db783476e65d6e94a56a64
styletab <katherinetab@gmail.com>
2016-10-13 13:57:13 -0400

    Wrap fs.readFile in a Promise
    Define a wrapper function for fs.readfile to readability to aid
    readability.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -6,10 +6,23 @@ const fs = require('fs');
 
 const filename = process.argv[2] || '';
 // by passing a blank string we'll get the following error: { [Error: ENOENT: no such file or directory, open ''] errno: -2, code: 'ENOENT', syscall: 'open', path: '' }
-
-fs.readFile(filename,(error, data) => {
-  if (error) {
-    return console.error(error);
-  }
-  console.log(`${filename} is ${data.length} bytes long`); // <-- this is a template literal
+const readFile = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename,(error, data) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(data);
+  });
 });
+
+};
+
+const logMessage = (data) => {
+    console.log(`${filename} is ${data.length} bytes long`); // <-- this is a template literal
+  };
+
+readFile(filename)
+.then(logMessage)
+.catch(console.error)
+;
