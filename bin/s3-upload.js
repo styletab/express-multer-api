7341db092b634de1a7d8c71cf923266f361a3425
styletab <katherinetab@gmail.com>
2016-10-13 15:02:32 -0400

    Determine file/mime-type
    Used the file-type module to determine the content type on upload
    (mime-type.)
    
    Define a function that gives us a default file type and
     mime type in the event that the file-type module is given a file
     type that is not supported.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+const fileType = require('file-type');
 
 const filename = process.argv[2] || '';
 // by passing a blank string we'll get the following error: { [Error: ENOENT: no such file or directory, open ''] errno: -2, code: 'ENOENT', syscall: 'open', path: '' }
@@ -16,11 +17,29 @@ const readFile = (filename) => {
 
 };
 
-const logMessage = (data) => {
-    console.log(`${filename} is ${data.length} bytes long`); // <-- this is a template literal
+// return a default file type in the case that that fileType is given an unsupported filetype to read
+const mimeType = (data) => {
+  return Object.assign({
+    ext: 'bin',
+    mime: 'application/octet-stream', // this is the safest default file type for anything . it's a binary file that has info in it (bytes)
+  }, fileType(data));
+
+};
+
+// this is going to return an object with the file and mime type
+const parseFile = (fileBuffer) => {
+  let file = mimeType(fileBuffer);
+  file.data = fileBuffer;
+  return file;
+};
+
+const logMessage = (file) => {
+  //  let typeData = fileType(data); // data is the contents of filename that was passed through readFile.
+    console.log(`${filename} is ${file.data.length} bytes long and is of mime ${file.mime}`); // <-- this is a template literal
   };
 
 readFile(filename)
+.then(parseFile)
 .then(logMessage)
 .catch(console.error)
 ;
